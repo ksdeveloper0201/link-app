ee3fc3734bdae6ba1657b88ff197d5ff661ac4e2
ksdeveloper0201 <ksdeveloper0201@gmail.com>
2025-05-03 01:53:32 +0900

    新規登録Button追加

---
+++ components/auth/LoginForm.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+
+export const LoginForm = () => {
+    return (
+        <Button
+            onClick={() => {}}
+            variant="link"
+            className="font-normal w-full"
+        >
+            新規登録
+        </Button>
+    );
+};
